59351ff503ccd36628cb331875fcb7edf7a090a8
Manjeet <twinstar1318@gmail.com>
2026-08-29 01:06:18 +0530

    new chges2

---
+++ Backend_02/src/index.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+// Method1:-
+
+// import express from "express";
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants.js";
+
+// const app=express();
+
+// (async()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         console.log("MongoDB connected successfully");
+
+
+//         app.on("error", (error)=>{
+//             console.error("EXPRESS ERROR:", error);
+//             throw error;
+//         });
+
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+
+//     }catch(error){
+//         console.log("ERROR:", error);
+//         throw error;
+//     }
+// })()
