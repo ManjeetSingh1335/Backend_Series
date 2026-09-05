33d8ddbe8b81254def196463ecf78b9c78f91fac
Manjeet <twinstar1318@gmail.com>
2026-09-05 20:25:42 +0530

    ..

--- Backend_02/src/models/user.model.js
+++ Backend_02/src/models/user.model.js
@@ -60,7 +60,7 @@ userSchema.methods.isPasswordCorrect=async function(password){
 }
 
 userSchema.methods.generateAccessToken=function(){
-    jwt.sign(
+    return jwt.sign(
         //payload
         {
             _id: this.id,
