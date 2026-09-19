a8480cb028a272f6dcbdd64d66e538b539187924
Manjeet <twinstar1318@gmail.com>
2026-09-19 20:04:42 +0530

    Api testing done with postman

--- Backend_02/src/db/db_index.js
+++ Backend_02/src/db/db_index.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
-import {DB_NAME} from "../constants.js";
+import { DB_NAME } from "../constants.js";
 
 const connectDB=async()=>{
     try{
-        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`\n MongoDB connected successfully!! DB HOST: ${connectionInstance.connection.host} \n`);
+        const connectionInstance=await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}`
+        );
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     }catch(error){
         console.error("MONGODB Connection Failed:", error);
         process.exit(1);
-    } 
-}
+    }
+};
 
 export default connectDB;
