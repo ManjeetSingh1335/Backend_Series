f038f7d3894fc1ec49acc9f7965d1a3c4b3fbd2c
Manjeet <twinstar1318@gmail.com>
2026-09-20 19:09:46 +0530

    all setup of accessToken , refreshToken and logout system done !!

--- Backend_02/src/routes/user.routes.js
+++ Backend_02/src/routes/user.routes.js
@@ -1,8 +1,11 @@
 import {Router} from "express";
 import {
+    loginUser,
+    logoutUser,
     registerUser,
 } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
+import {verifyJWT} from "../middlewares/auth.middleware.js";
 
 
 
@@ -21,5 +24,9 @@ router.route("/register").post(
     ]),
     registerUser
 )
+
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(verifyJWT, logoutUser)
 
 export default router;
