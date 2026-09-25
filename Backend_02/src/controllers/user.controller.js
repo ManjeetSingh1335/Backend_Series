813d548d17ea4a95bb96afa55b1b6bcb1803b834
Manjeet <twinstar1318@gmail.com>
2026-09-26 01:15:26 +0530

    experimenting with bug

--- Backend_02/src/controllers/user.controller.js
+++ Backend_02/src/controllers/user.controller.js
@@ -178,13 +178,13 @@ const refreshAccessToken=asyncHandler(async(req, res)=>{
         }
 
    
-        const {accessToken, newrefreshToken}=await generateAccessAndRefreshToken(user._id)
+        const {accessToken, refreshToken: newRefreshToken}=await generateAccessAndRefreshToken(user._id)
 
         return res
         .status(200)
         .cookie("accessToken", accessToken, options)
-        .cookie("refreshToken", newrefreshToken, options)
-        .json(new ApiResponse(200, {accessToken, refreshToken: newrefreshToken}, "Access token refreshed"))
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(new ApiResponse(200, {accessToken, refreshToken: newRefreshToken}, "Access token refreshed"))
 
     }catch(error){
         throw new ApiError(401, error?.message || "Invalid refresh token")
